0ea28de97d5f5aa1a7e711e738dc7f07587a00a8
wiemkouki <79139149+wiemkouki@users.noreply.github.com>
2021-03-22 16:40:35 +0100

    m

--- backend/migrations/20210322153124-create-panier.js
+++ backend/migrations/20210322153124-create-panier.js
@@ -18,8 +18,16 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
-      }
-    });
+      },
+      id_client:
+      {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'client',
+          key: 'id'
+        },
+    }});
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('paniers');
--- backend/models/fiche_tech.js
+++ backend/models/fiche_tech.js
@@ -4,13 +4,9 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class fiche_tech extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  
     static associate(models) {
-      // define association here
+   fiche_tech.belongsTo(models.produit,{foreignKey: 'id_produit', as:'produit'})
     }
   };
   fiche_tech.init({
