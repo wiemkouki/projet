07849e53ef5c5c044de1e83ac17c4a7e9dbd7c41
rahma_kl <58667277+rahmaKL@users.noreply.github.com>
2021-03-22 15:53:03 +0100

    kk

--- backend/models/produit.js
+++ backend/models/produit.js
@@ -6,13 +6,8 @@ module.exports = (sequelize, DataTypes) => {
   class produit extends Model {
     
     static associate(models) {
-<<<<<<< Updated upstream
       Produit.belongsTo(models.panier,{ foreignKey: 'id_panier', as: 'panier' });
       Produit.hasOne(models.fiche_tech, { foreignKey: 'id_produit', as: 'fiche_tech' });
-=======
-      produit.belongsTo(models.panier,{ foreignKey: 'id_panier', as: 'panier' });
-      produit.hasOne(models.fiche_tech, { foreignKey: 'id_produit', as: 'fiche_tech' });
->>>>>>> Stashed changes
     }
     
   };
