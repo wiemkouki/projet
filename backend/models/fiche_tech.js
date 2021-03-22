25b66e5e3cb6db99b183bc7fdd5a33e4a255f24c
rahma_kl <58667277+rahmaKL@users.noreply.github.com>
2021-03-22 16:34:45 +0100

    ad

--- backend/models/fiche_tech.js
+++ backend/models/fiche_tech.js
@@ -4,13 +4,9 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class fiche_tech extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+   
     static associate(models) {
-      // define association here
+      fiche_tech .belongsTo(models.panier,{ foreignKey: 'id_panier', as: 'panier' });
     }
   };
   fiche_tech.init({
