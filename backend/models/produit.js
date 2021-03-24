a0cf78f41bd81793b562e19a618e812633c1547b
rahma_kl <58667277+rahmaKL@users.noreply.github.com>
2021-03-24 14:28:35 +0100

    az

--- backend/models/produit.js
+++ backend/models/produit.js
@@ -6,15 +6,11 @@ module.exports = (sequelize, DataTypes) => {
   class Produit extends Model {
 
     static associate(models) {
-<<<<<<< HEAD
-      Produit.hasMany(models.Commande, { foreignKey: 'id_commande', as: 'commande' });
-      Produit.hasMany(models.notePdt, { foreignKey: 'id_notePdt', as: 'doc_notePdt' });
 
-=======
-   Produit.hasMany(models.Commande, { foreignKey: 'id_commande', as: 'commande' });
-   Produit.hasMany(models.images_produit, { foreignKey: 'id_images_produit', as: 'images_produit' });
-   Produit.hasMany(models.notes_produit, { foreignKey: 'id_notes_produit', as: 'notes_produit' });
->>>>>>> b2fcd5658d185eb9339418a925fe84eef5a80257
+      Produit.hasMany(models.notePdt, { foreignKey: 'id_notePdt', as: 'doc_notePdt' });
+      Produit.hasMany(models.Commande, { foreignKey: 'id_commande', as: 'commande' });
+      Produit.hasMany(models.images_produit, { foreignKey: 'id_images_produit', as: 'images_produit' });
+
     }
   };
   Produit.init({
