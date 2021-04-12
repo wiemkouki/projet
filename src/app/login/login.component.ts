bf2fea8d442fd9f1421f9071c7b93797222ac7fa
rahma_kl <58667277+rahmaKL@users.noreply.github.com>
2021-04-12 15:05:25 +0100

    kou

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from '../user-service.service';
-import  { Observable } from 'rxjs';
-import { any } from 'sequelize/types/lib/operators';
-
-import { User } from 'backend/models/user';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +9,7 @@ import { User } from 'backend/models/user';
 })
 
 
-export class LoginComponent implements OnInit 
-{
+export class LoginComponent implements OnInit {
   form: any = {
     email: null,
     password: null,
@@ -23,135 +19,60 @@ export class LoginComponent implements OnInit
   errorMessage = '';
   roles: string[] = [];
 
-
-
-
-
-
-
-
-  // users: User = {
-  //   username: '',
-  //   password: '',
-  //   role: '',
-  //   email: ''
-  // };
-  // submitted = false;
-
-  constructor(private userService: UserServiceService) 
-  {}
+  constructor(private userService: UserServiceService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
 
+  onSubmit(): void {
+    const { username, password } = this.form;
+
+    this.userService.login(username, password).subscribe(
+      data => {
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   saveUser(): void {
-//     const data = {
-//       username: this.users.username,
-//       password: this.users.password,
-//       role: this.users.role,
-//       email: this.users.email
-//     };
-
-
-//     this.userService.create(data)
-//     .subscribe(
-//       response => {
-//         console.log(response);
-//         this.submitted = true;
-//       },
-//       error => {
-//         console.log(error);
-//       });
-// }
-
-// SignIn(): void {
-//     this.submitted = false;
-//     this.users = {
-//       username: '',
-//       password: '',
-//       role: '',
-//       email:''
-//     };
-//   }
-
-// }
-
-
-
-
-
-
-
-
-
-
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.router.navigate(['/profil'])
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    );
   }
 
+  reloadPage(): void {
+    window.location.reload();
+  }
+}
 
 
 
 
 
 
-  // public users: any=[];
-  
 
-//   constructor(private userService: UserServiceService) 
-//   {
-// console.log('welcome');
-// this.SignIn();
-//  }
 
-//   ngOnInit()
-//   {
-//     this.userService.showSuccess().subscribe(function(data) {
-//       this.users.push(data);
-//     });
-//   }
-   
 
-  
-//   SignIn(){
-//     this.userService.SignIn(data).subscribe(function(data) {
-//       console.log(data);
-      
-//     }); }
-//   }
 
-// function data(data: any) {
-//   throw new Error('Function not implemented.');
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
