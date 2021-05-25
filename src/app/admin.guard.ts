485b096831143757e5330219dd4bc68cf84cc3b7
rahma_kl <58667277+rahmaKL@users.noreply.github.com>
2021-05-25 11:05:38 +0100

    h

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from './services/auth.service';
+export class User {
+  constructor(
+    public token: string,
+    public role: string,
+
+
+  ) {}
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -13,11 +21,16 @@ constructor(private auth :AuthService,private router: Router){}
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
       if (this.auth.isLoggedIn()){
-        // localStorage.setItem();
-        return true;
-  }
+        localStorage.setItem('role',JSON.stringify('token.role') );
+        localStorage.getItem('role');
+        if (localStorage.getItem('role')=="Administrateur"){
+          return true;
+        }
+        return this.router.parseUrl("/admin");
+
+  }else{
   window.alert('You don\'t have permission to view this page');
-  return this.router.parseUrl("/login");
-}
+  return this.router.parseUrl("/");
+}}
 
 }
