42a201ef07ab1a4022ede14e73e1831217f3fa4f
rahma_kl <58667277+rahmaKL@users.noreply.github.com>
2021-06-11 11:03:20 +0100

    UPDATE profile

--- backend/routes/produit.js
+++ backend/routes/produit.js
@@ -2,13 +2,18 @@ var express = require("express");
 var router = express.Router();
 var multer = require("multer");
 
-const { Produit,fiche_teches ,images_produit,categorie} = require("../models");
+const {
+  Produit,
+  fiche_teches,
+  images_produit,
+  categorie,
+} = require("../models");
 
 const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png',
-  'image/bmp': 'bmp'
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+  "image/bmp": "bmp",
 };
 
 const prepareResponse = (response, status, body, type) => {
@@ -17,41 +22,54 @@ const prepareResponse = (response, status, body, type) => {
   response.status(status).send(body);
 };
 
-
 var storage = multer.diskStorage({
-  destination:function(req,file,cb){
-      cb(null, './uploads/image_produit');
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/image_produit");
   },
-  filename:function(req,file,cb){
-    cb(null, (file.originalname) + "." + MIME_TYPES[file.mimetype]);
-  }
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + "." + MIME_TYPES[file.mimetype]);
+  },
 });
 
-var images = multer({storage:storage}).single('file');
+var images = multer({ storage: storage }).single("file");
 //get Product by ID
 
 router.get("/getPdt/:id", async function (req, res, next) {
   let id = req.params.id;
-  const produit = await Produit.findByPk(id,
-    { attributes: ["id","libelle", "marque", "prix", "max_rating","description","createdAt",
-    "updatedAt"],
-  include: [{ model: categorie, attributes: ['id'], as: 'categorie'}],
- }
- );
+  const produit = await Produit.findByPk(id, {
+    attributes: [
+      "id",
+      "libelle",
+      "marque",
+      "prix",
+      "max_rating",
+      "description",
+      "createdAt",
+      "updatedAt",
+    ],
+    include: [{ model: categorie, attributes: ["id"], as: "categorie" }],
+  });
 
   prepareResponse(res, 200, produit, "application/json");
 });
 
-
 //get ALL products
 router.get("/getAll", function (req, res, next) {
-  Produit.findAll({ attributes: ["id","libelle", "marque", "prix", "max_rating","description","createdAt", "updatedAt"],
-  where: {
-    is_deleted: 0
-  } }
-
-
-  )
+  Produit.findAll({
+    attributes: [
+      "id",
+      "libelle",
+      "marque",
+      "prix",
+      "max_rating",
+      "description",
+      "createdAt",
+      "updatedAt",
+    ],
+    where: {
+      is_deleted: 0,
+    },
+  })
     .then((produit) => {
       prepareResponse(res, 200, produit, "application/json");
     })
@@ -67,117 +85,131 @@ router.get("/getAll", function (req, res, next) {
     });
 });
 
-//get Product by categorie
-router.get("/getP/:id", async function (req, res, next) {
-  let id = req.params.id;
-  const produit = await Produit.findByPk(id,
-    { attributes: ["id","libelle", "marque", "prix", "max_rating","description","createdAt",
-    "updatedAt"],
-  include: [{ model: categorie, attributes: ['id'], as: 'categorie'}],
-     }
- );
+//GET PRODUCTS BY Category
+router.get("/getP", async function (req, res, next) {
+ 
+  const produit = await Produit.findAll({
+    attributes: [
+      "id",
+      "libelle",
+      "marque",
+      "prix",
+      "max_rating",
+      "description",
+
+    ],
+    include: [
+      {
+        model: categorie,
+        attributes: ["nom_cat"],
+        as: "categorie",
+        order: [["nom_cat", "ASC"]],
+      },
+    ],
+  });
 
   prepareResponse(res, 200, produit, "application/json");
 });
 
-
 // create Produit
-router.post("/createP", function (req, res, next) {
-  Produit.findOne(
-    {
-      attributes: ['libelle'],
-      where: {
-        libelle: req.body.libelle,
-      },
-    }
-  ).then(async (pdt) => {
-    if (pdt)
-    {
-      const response = {
-        success: false,
-        message: "Produit already exist !",
-      };
-      prepareResponse(res, 500, response, "application/json");
-    } else {
-      let { id,libelle,marque,prix,max_rating,description,} = req.body;
-      const produit = await Produit.create({
-        id,
-        libelle,
-        marque,
-        prix,
-        max_rating,
-        description,
-        is_deleted: false,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      })
-      fiche_teches.create({
-        id_produit:produit.id,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      })
-      const response = {
-        success: true,
-        message: "Produit created successfully.",
-      };
-      prepareResponse(res, 200, response, "application/json");
-    }
+router.post("/createP/:id", function (req, res, next) {
+
+  categorie.findOne({attributes:["id","nom_cat", "famille"],
+       where: { id: req.params.id  } }).then((categorie)=>{
+  Produit.findOne({
+    attributes: ["libelle"],
+    where: {
+      libelle: req.body.libelle,
+    },
   })
-  .catch((error) => console.log(error));
-});
-
-// Ajouter IMAGES PRODUITS
-router.post('/uploadPic/:id', function(req,res,next){
-  images(req, res, async function(err)
-  {
-      if (err)
-      {  console.log(err);
-        return res.status(501).json({error:err});
-      }
-      else{
-
-      images_produit.create({
-              url:(req.file.originalname) + "." + MIME_TYPES[req.file.mimetype],
-              is_deleted: false,
-              createdAt: new Date(),
-              updatedAt: new Date(),
-              id_produit:req.params.id
-            });
-            // })
-      //do all database record saving activity
-      return res.json({originalname:req.file.originalname, uploadname:req.file.filename});
-
-          }
-   });
-});
-
-//UPDATE
-
-router.put("/updateP/:id", function (req, res) {
-  let id = req.params.id;
-  Produit.findByPk(id,
-    { attributes: ["id"] })
-    .then((pdt) => {
-      try {
-        let { id, libelle, marque, prix, max_rating, description } = req.body;
-        pdt.update({
+    .then(async (pdt) => {
+      if (pdt) {
+        const response = {
+          success: false,
+          message: "Produit already exist !",
+        };
+        prepareResponse(res, 500, response, "application/json");
+      } else {
+        let { id, libelle, marque, prix, max_rating, description,quantite } = req.body;
+        const produit = await Produit.create({
           id,
           libelle,
           marque,
           prix,
           max_rating,
           description,
-          updatedAt: new Date()
-        })
-          .then((pdt) =>
-            prepareResponse(res, 200, { success: true }, "application/json")
-          )
-          .catch((error) => console.log(error));
-      } catch (error) {
-        console.log(error);
-        prepareResponse(res, 500, { success: false }, "application/json");
+          quantite,
+          id_categorie:categorie.id,
+          is_deleted: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        });
+        fiche_teches.create({
+          id_produit: produit.id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        });
+        const response = {
+          success: true,
+          message: "Produit created successfully.",
+        };
+        prepareResponse(res, 200, response, "application/json");
       }
-    });
+    })
+  })
+    .catch((error) => console.log(error));
+});
+
+// Ajouter IMAGES PRODUITS
+router.post("/uploadPic/:id", function (req, res, next) {
+  images(req, res, async function (err) {
+    if (err) {
+      console.log(err);
+      return res.status(501).json({ error: err });
+    } else {
+      images_produit.create({
+        url: req.file.originalname + "." + MIME_TYPES[req.file.mimetype],
+        is_deleted: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        id_produit: req.params.id,
+      });
+      // })
+      //do all database record saving activity
+      return res.json({
+        originalname: req.file.originalname,
+        uploadname: req.file.filename,
+      });
+    }
+  });
+});
+
+//UPDATE
+
+router.put("/updateP/:id", function (req, res) {
+  let id = req.params.id;
+  Produit.findByPk(id, { attributes: ["id"] }).then((pdt) => {
+    try {
+      let { id, libelle, marque, prix, max_rating, description } = req.body;
+      pdt
+        .update({
+          id,
+          libelle,
+          marque,
+          prix,
+          max_rating,
+          description,
+          updatedAt: new Date(),
+        })
+        .then((pdt) =>
+          prepareResponse(res, 200, { success: true }, "application/json")
+        )
+        .catch((error) => console.log(error));
+    } catch (error) {
+      console.log(error);
+      prepareResponse(res, 500, { success: false }, "application/json");
+    }
+  });
 });
 //DELETE Produit
 
@@ -186,7 +218,7 @@ router.get("/delete/:id", function (req, res) {
   Produit.findByPk(id, { attributes: ["id"] }).then((produits) => {
     try {
       produits.update({
-        is_deleted: true
+        is_deleted: true,
       });
       prepareResponse(res, 200, { success: true }, "application/json");
     } catch (error) {
