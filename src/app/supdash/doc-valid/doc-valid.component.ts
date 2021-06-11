02cc156a19a5cca72ed6da671083dbfb2b308c4a
wiemkouki <79139149+wiemkouki@users.noreply.github.com>
2021-06-11 14:39:05 +0100

    update

--- src/app/supdash/doc-valid/doc-valid.component.ts
+++ src/app/supdash/doc-valid/doc-valid.component.ts
@@ -41,19 +41,6 @@ getDoc() {
 }
 
 open(content ,id:number) {
-  // alert(id);
-  const path= $("#photo_"+id).val();
-var appendp= "" ;
-appendp+="<img src='http://localhost:3000/backend/uploads/"+path+"'>";
-
-$("#preview").html("");
-$("#preview").text("appendp");
-
-
-
-
-
-
   this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
     this.closeResult = `Closed with: ${result}`;
   }, (reason) => {
